a9d0619d52413f16d783ee7474f296e9c74f7c26
Aahrizz <upjeffrey2outlook.com>
2024-11-12 15:51:18 +0300

    new files added

---
+++ functions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int one = 9;
+    int two = 6;
+
+    int sum  =  one + two;
+
+    printf("The sum of the two pre-input numbers is: %d\n", sum);
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Today I was late. Explains why I cannot subscribe to the thought of living in Thika road.\n");
+}
+
